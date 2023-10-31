dc43895028163ae7860ec50af61287f0e3f3e8fc
Stefan van Ieperen <stefan.van.ieperen@gmail.com>
2023-10-31 09:36:57 +0100

    Fix date being cut off near top

--- components/DateThumbnail.tsx
+++ components/DateThumbnail.tsx
@@ -9,7 +9,8 @@ interface Props {
 const DateThumbnail: React.FC<Props> = ({ coverSource}) => {
     return (
       <View className={'flex-start  m-2 justify-center bg-gray-200 rounded-lg w-32 h-32'}>
-        <Text className={'text-primary text-center font-extrabold mt-2'}>
+        {/* <Text textBreakStrategy={"simple"} className={"pt-1 font-extrabold text-center font-extrabold text-6xl"}>31</Text> */}
+        <Text className={'text-primary pt-1 text-center font-extrabold mt-2'}>
           <Text className={'text-5xl'}>{coverSource.getDate()+"\n"}</Text>
           <Text className={'text-xl'}>{coverSource.toLocaleDateString('default', {month: 'short'})}</Text>
         </Text>
