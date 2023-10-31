b38b3aa440f39a979c3dad7e9efeaea2e90a4d9f
Maikel <45266691+2maikel1@users.noreply.github.com>
2023-10-31 15:36:55 +0100

    started work on pulling links from database

--- app/mp3_player.tsx
+++ app/mp3_player.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import SoundPlayer from 'react-native-sound-player';
 import { Icon } from 'react-native-elements';
-import Slider from '@react-native-community/slider';
+import { supabase } from '../lib/supabase';
 
 const tracks = [
   {
@@ -15,20 +15,8 @@ const tracks = [
 
 const AudioPlayer = () => {
   const [isPlaying, setIsPlaying] = useState(false);
-  const [position, setPosition] = useState(0);
   const [duration, setDuration] = useState(0);
-  const [sliderValue, setSliderValue] = useState(0);
-
-  const handleSeek = () => {
-    try {
-      void SoundPlayer.getInfo().then((info) => {
-        setPosition(info.duration - info.currentTime);
-        setSliderValue(info.currentTime / info.duration);
-      });
-    } catch (error) {
-      console.error('Error while seeking:', error);
-    }
-  };
+  const [audioLink, setAudioLink] = useState(''); 
 
   const getDuration = () => {
     try {
@@ -53,7 +41,6 @@ const AudioPlayer = () => {
     try {
       SoundPlayer.pause();
       setIsPlaying(false);
-      handleSeek();
     } catch (e) {
       console.error('Error pausing track:', e);
     }
@@ -70,10 +57,24 @@ const AudioPlayer = () => {
   useEffect(() => {
     const setupAndAddTracks = async () => {
       try {
-        SoundPlayer.loadUrl(tracks[0].url);
-        SoundPlayer.play();
-        getDuration();
-        setIsPlaying(true);
+        // Fetch the audio link from Supabase
+        const { data, error } = await supabase
+          .from('audio')
+          .select('link')
+          .single();
+
+        if (error !== null) {
+          console.error('Error fetching audio link:', error);
+        } else if (data !== null) {
+          const audioUrl = data.link;
+          setAudioLink(audioUrl); // Set the audio link in the state
+
+          // Load and play the audio using the extracted URL
+          SoundPlayer.loadUrl(audioUrl);
+          SoundPlayer.play();
+          getDuration();
+          setIsPlaying(true);
+        }
       } catch (e) {
         console.error('Error setting up SoundPlayer:', e);
       };
@@ -87,21 +88,8 @@ const AudioPlayer = () => {
       <Image source={{ uri: tracks[0].artwork }} className='h-64 w-64' />
       <Text className='mt-8 font-bold text-20'>{tracks[0].title}</Text>
       <Text className='mt-4 text-20'>{tracks[0].artist}</Text>
-      {/* <Slider 
-        className='flex' 
-        value={sliderValue} 
-        onValueChange={(value) => { setSliderValue(value); }} 
-        onSlidingComplete={(value) => { seekTo(value); }}
-      /> */}
-      <Slider
-  style={{width: 200, height: 40}}
-  minimumValue={0}
-  maximumValue={1}
-  minimumTrackTintColor="#FFFFFF"
-  maximumTrackTintColor="#000000"
-/>
       <View className='flex-row m-10'>
-        <TouchableOpacity className='flex rounded-full' onPress={pauseTrack}>
+        <TouchableOpacity className='flex rounded-full' onPress={() => { seekTo(0); }}>
           <Icon name="skip-previous" size={70} color="#00DEAD" />
         </TouchableOpacity>
         {isPlaying ? (
@@ -113,7 +101,7 @@ const AudioPlayer = () => {
             <Icon name="play-circle-outline" size={70} color="#00DEAD" />
           </TouchableOpacity>
         )}
-        <TouchableOpacity className='flex rounded-full' onPress={pauseTrack}>
+        <TouchableOpacity className='flex rounded-full' onPress={() => { seekTo(0.5); }}>
           <Icon name="skip-next" size={70} color="#00DEAD" />
         </TouchableOpacity>
       </View>
