fffb5996777e33070599a9d2138e80aa3103190f
Maikel <45266691+2maikel1@users.noreply.github.com>
2023-10-31 14:08:45 +0100

    broken

--- app/mp3_player.tsx
+++ app/mp3_player.tsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
-import TrackPlayer from 'react-native-track-player';
+import SoundPlayer from 'react-native-sound-player';
 import { Icon } from 'react-native-elements';
-import { Seekbar } from 'react-seekbar'
+import Slider from '@react-native-community/slider';
 
 const tracks = [
   {
-    id: 'track1',
     url: 'https://oeybruqyypqhrcxcgkbw.supabase.co/storage/v1/object/public/audio/sample/morgan.mp3',
     title: 'Morgan Freeman speech',
     artist: 'NU.nl',
@@ -14,58 +13,70 @@ const tracks = [
   },
 ];
 
-const AudioPlayer: React.FC = () => {
+const AudioPlayer = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [position, setPosition] = useState(0);
   const [duration, setDuration] = useState(0);
+  const [sliderValue, setSliderValue] = useState(0);
 
-  const handleSeek = async () => {
+  const handleSeek = () => {
     try {
-      const progress = await TrackPlayer.getProgress();
-      console.log(progress);
-      setPosition(progress.duration - progress.position);
+      void SoundPlayer.getInfo().then((info) => {
+        setPosition(info.duration - info.currentTime);
+        setSliderValue(info.currentTime / info.duration);
+      });
     } catch (error) {
       console.error('Error while seeking:', error);
     }
   };
 
-  const getDuration = async () => {
-    const progress = await TrackPlayer.getProgress();
-    console.log(progress.duration)
-    setDuration(progress.duration);
-  }
-
-  const resumeTrack = async () => {
+  const getDuration = () => {
     try {
-      await TrackPlayer.play();
+      void SoundPlayer.getInfo().then((info) => {
+        setDuration(info.duration);
+      });
+    } catch (error) {
+      console.error('Error getting duration:', error);
+    }
+  };
+
+  const resumeTrack = () => {
+    try {
+      SoundPlayer.play();
       setIsPlaying(true);
     } catch (e) {
       console.error('Error resuming track:', e);
     }
   };
 
-  const pauseTrack = async () => {
+  const pauseTrack = () => {
     try {
-      await TrackPlayer.pause();
+      SoundPlayer.pause();
       setIsPlaying(false);
-      await handleSeek()
+      handleSeek();
     } catch (e) {
       console.error('Error pausing track:', e);
+    }
+  };
+
+  const seekTo = (value: number) => {
+    try {
+      SoundPlayer.seek(value * duration);
+    } catch (e) {
+      console.error('Error seeking:', e);
     }
   };
 
   useEffect(() => {
     const setupAndAddTracks = async () => {
       try {
-        await TrackPlayer.setupPlayer();
-        await TrackPlayer.reset();
-        await TrackPlayer.add(tracks);
-        await TrackPlayer.play();
-        await getDuration();
+        SoundPlayer.loadUrl(tracks[0].url);
+        SoundPlayer.play();
+        getDuration();
         setIsPlaying(true);
       } catch (e) {
-        console.error('Error setting up TrackPlayer:', e);
-      }
+        console.error('Error setting up SoundPlayer:', e);
+      };
     };
 
     void setupAndAddTracks();
@@ -76,21 +87,33 @@ const AudioPlayer: React.FC = () => {
       <Image source={{ uri: tracks[0].artwork }} className='h-64 w-64' />
       <Text className='mt-8 font-bold text-20'>{tracks[0].title}</Text>
       <Text className='mt-4 text-20'>{tracks[0].artist}</Text>
-      {/* <Seekbar /> */}
+      {/* <Slider 
+        className='flex' 
+        value={sliderValue} 
+        onValueChange={(value) => { setSliderValue(value); }} 
+        onSlidingComplete={(value) => { seekTo(value); }}
+      /> */}
+      <Slider
+  style={{width: 200, height: 40}}
+  minimumValue={0}
+  maximumValue={1}
+  minimumTrackTintColor="#FFFFFF"
+  maximumTrackTintColor="#000000"
+/>
       <View className='flex-row m-10'>
-        <TouchableOpacity className='flex rounded-full' onPress={() => {pauseTrack().catch(e => {console.log(e)})}}>
+        <TouchableOpacity className='flex rounded-full' onPress={pauseTrack}>
           <Icon name="skip-previous" size={70} color="#00DEAD" />
         </TouchableOpacity>
         {isPlaying ? (
-          <TouchableOpacity className='flex rounded-full' onPress={() => {pauseTrack().catch(e => {console.log(e)})}}>
+          <TouchableOpacity className='flex rounded-full' onPress={pauseTrack}>
             <Icon name="pause-circle-outline" size={70} color="#00DEAD" />
           </TouchableOpacity>
         ) : (
-          <TouchableOpacity className='flex rounded-full' onPress={() => {resumeTrack().catch(e => {console.log(e)})}}>
+          <TouchableOpacity className='flex rounded-full' onPress={resumeTrack}>
             <Icon name="play-circle-outline" size={70} color="#00DEAD" />
           </TouchableOpacity>
         )}
-        <TouchableOpacity className='flex rounded-full' onPress={() => {pauseTrack().catch(e => {console.log(e)})}}>
+        <TouchableOpacity className='flex rounded-full' onPress={pauseTrack}>
           <Icon name="skip-next" size={70} color="#00DEAD" />
         </TouchableOpacity>
       </View>
