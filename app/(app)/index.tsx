ce18a08e9f7d728a9c1451e5a11d5a78423f3144
Maikel <45266691+2maikel1@users.noreply.github.com>
2023-11-17 21:18:35 +0100

    made it fetch name for user to display on both profile page and index page

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,10 +1,12 @@
-import { ScrollView, View, Text, Image, Pressable } from 'react-native';
+import { ScrollView, View, Text, Image, Pressable, Alert } from 'react-native';
 import NewsThumbnail from '../../components/NewsThumbnail';
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import DateThumbnail from '../../components/DateThumbnail';
 import { Icon } from 'react-native-elements';
 import { Link } from 'expo-router';
 import Mp3_player_minum from './Mp3_player_minum'
+import { getFullName } from '../../lib/supabase';
+import { SupabaseUserSession } from '../../contexts/user_session'
 
 
 function createDateThumbnails(amount: number){
@@ -30,20 +32,47 @@ function createNewsThumbnails(amount: number){
 }
 
 export default function homePage() {
+  const [fullName, setFullName] = useState('');
+  const userContext = useContext(SupabaseUserSession);
+  const userId = userContext.session?.user.id; 
+
+  useEffect(() => {
+    const fetchFullName = async () => {
+      try {
+        if (userId) { // Check if userId is defined
+          const name = await getFullName(userId);
+          if (name !== null) {
+            setFullName(name);
+          } else {
+            // Handle the error or provide a default value
+            Alert.alert('Error fetching full name.');
+          }
+        }
+      } catch (error) {
+        console.error('Error fetching full name:', error.message);
+        Alert.alert('Error fetching full name.');
+      }
+    };
+  
+    fetchFullName();
+  }, [userId]);
+  
+
   return (
     <View className={'flex w-full justify-center'}>
       <View className="absolute top-8 right-4 z-10">
-      <Link href={'/profile'} asChild>
-        <Pressable>
-          <Icon name="account-circle" size={36} color="black" />
-        </Pressable>
-      </Link>
-        
+        <Link href={'/profile'} asChild>
+          <Pressable>
+            <Icon name="account-circle" size={36} color="black" />
+          </Pressable>
+        </Link>
       </View>
       <ScrollView>
         <View className={"bg-primary pt-8 pb-4 px-2"}>
           <Text className={'mt-4 mx-2 mb-2 text-4xl text-center font-semibold font-Poppins_600_semi_bold'}>Goedemorgen,</Text>
-          <Text className={'mx-2 mb-2 text-4xl text-center font-semibold font-Poppins_600_semi_bold'}>Stefan</Text>
+          <Text className={'mx-2 mb-2 text-4xl text-center font-semibold font-Poppins_600_semi_bold'}>
+            {typeof fullName === 'string' ? fullName : 'Loading...'}
+          </Text>
           <View>
             <View id={"background"} className={"mx-2 rounded-xl bg-background flex justify-center"}>
               <Image source={require("../../assets/waveform.png")} className={"m-4 h-48 w-10/12 self-center"}></Image>
@@ -57,14 +86,14 @@ export default function homePage() {
             </View>
           </View>
         </View>
-      <Text className={'mt-4 mx-2 text-2xl font-semibold font-Poppins_700_bold'}>Overige Topix</Text>
-      <ScrollView horizontal showsHorizontalScrollIndicator={false} >
-        { createNewsThumbnails(10) }
-      </ScrollView>
-      <Text className={'mt-4 mx-2 text-2xl font-semibold font-Poppins_700_bold'}>Terugluisteren</Text>
-      <ScrollView horizontal showsHorizontalScrollIndicator={false} >
-        { createDateThumbnails(10) }
-      </ScrollView>
+        <Text className={'mt-4 mx-2 text-2xl font-semibold font-Poppins_700_bold'}>Overige Topix</Text>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false} >
+          { createNewsThumbnails(10) }
+        </ScrollView>
+        <Text className={'mt-4 mx-2 text-2xl font-semibold font-Poppins_700_bold'}>Terugluisteren</Text>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false} >
+          { createDateThumbnails(10) }
+        </ScrollView>
       </ScrollView>
       <View className='absolute bottom-0 w-full'>
         <Mp3_player_minum></Mp3_player_minum>
--- app/(app)/profile.tsx
+++ app/(app)/profile.tsx
@@ -1,44 +1,70 @@
-import React, { useContext } from 'react';
-import { View, Text, Pressable } from 'react-native';
+import React, { useContext, useEffect, useState } from 'react';
+import { View, Text, Pressable, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { signOut } from '../../lib/supabase';
+import { signOut, getFullName } from '../../lib/supabase';
+import { SupabaseUserSession } from '../../contexts/user_session'
 
 export default function ProfilePage() {
-  const navigation = useNavigation();
-
-  const handleLogout = async () => {
-    try {
-      await signOut();
-      // Navigate to the login page or replace 'LoginPage' with the desired screen name
-      navigation.navigate('welcome');
-    } catch (e) {
-      console.error('Error logging out:', e.message);
-    }
-  };
-
-  const handleTopicSelection = () => {
-    // Navigate to the 'topicSelection' screen
-    navigation.navigate('topicSelection');
-  };
-
+    const navigation = useNavigation();
+    const [fullName, setFullName] = useState('');
+    const userContext = useContext(SupabaseUserSession);
+    const userId = userContext.session?.user.id; // Replace with the actual user ID from Supabase
+    
+  
+    const handleLogout = async () => {
+      try {
+        await signOut();
+        // Navigate to the login page or replace 'LoginPage' with the desired screen name
+        navigation.navigate('welcome');
+      } catch (e) {
+        console.error('Error logging out:', e.message);
+      }
+    };
+  
+    const handleTopicSelection = () => {
+      // Navigate to the 'topicSelection' screen
+      navigation.navigate('topicSelection');
+    };
+  
+    useEffect(() => {
+        console.log(userId)
+      const fetchFullName = async () => {
+        try {
+          if (userId) { // Check if userId is defined
+            const name = await getFullName(userId);
+            if (name !== null) {
+                setFullName(name);
+            } else {
+                // Handle the error or provide a default value
+                Alert.alert('Error fetching full name.');
+            }
+          }
+        } catch (error) {
+          console.error('Error fetching full name:', error.message);
+          Alert.alert('Error fetching full name.');
+        }
+      };
+  
+      void fetchFullName();
+    }, [userId]);
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-2xl font-semibold mb-4">Profile Page</Text>
+      <Text className="text-2xl font-semibold mb-4">Profiel Pagina</Text>
 
       {/* Display Name */}
-      <Text className="text-xl mb-2">Stefan</Text>
+      <Text className="text-xl mb-2">{fullName}</Text>
 
       {/* Topic Selection Button */}
       <Pressable onPress={handleTopicSelection}>
         <View className="bg-primary p-2 rounded-md mb-4">
-          <Text className="text-white">Select Topix</Text>
+          <Text className="text-white">Selecteer Topix</Text>
         </View>
       </Pressable>
 
       {/* Logout Button */}
       <Pressable onPress={handleLogout}>
         <View className="bg-primary p-2 rounded-md">
-          <Text className="text-white">Logout</Text>
+          <Text className="text-white">Uitloggen</Text>
         </View>
       </Pressable>
     </View>
