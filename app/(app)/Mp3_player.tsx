cdda3589d7b7d6dc399fc396ae3c0877ae55f4a9
Walter <walter.levens@gmail.com>
2023-11-23 14:13:25 +0100

    updated undefined bug

--- app/(app)/Mp3_player.tsx
+++ app/(app)/Mp3_player.tsx
@@ -1,12 +1,13 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
-import SoundPlayer from 'react-native-sound-player';
+import { SupabaseUserSession } from '../../contexts/user_session'
 import { Icon } from 'react-native-elements';
 import { AudioPlayerContext } from '../../contexts/audio_player';
 import { supabase } from '../../lib/supabase';
 
 const tracks = [
   {
+//   get url using: "https://topix.site:8000/storage/v1/object/public/audio/podcasts/{userid}/{date}_{userid}.mp3"
     url: 'https://oeybruqyypqhrcxcgkbw.supabase.co/storage/v1/object/public/audio/sample/morgan.mp3',
     title: 'Morgan Freeman speech',
     artist: 'NU.nl',
@@ -16,6 +17,9 @@ const tracks = [
 
 const AudioPlayer = () => {
   const audioContext = useContext(AudioPlayerContext);
+  const [podcast, setPodcast] = useState({url: "", title: "", artist: "", artwork: ""});
+  const userContext = useContext(SupabaseUserSession);
+  const userId = userContext.session?.user.id;
 
   useEffect(() => {
     void (async () => {
@@ -28,15 +32,45 @@ const AudioPlayer = () => {
   }, []);
 
   useEffect(() => {
+    async function getPodcast(){
+        // check user userid
+        // get current date
+        const date = new Date();
+        const currentDate = `${padTo2Digits(date.getDate())}-${padTo2Digits(date.getMonth() + 1)}-${date.getFullYear()}`;
+        console.log(currentDate);
+        // create url
+        const url = `https://topix.site:8000/storage/v1/object/public/audio/podcasts/${userId}/${currentDate}_${userId}.mp3`
+        console.log(url)
+        // come up with title and artist
+        const title = "Dagelijkse podcast";
+
+        const newPodcast = {
+            url: 'https://oeybruqyypqhrcxcgkbw.supabase.co/storage/v1/object/public/audio/sample/morgan.mp3',
+            title: title,
+            artist: currentDate,
+            artwork: 'https://cdn.britannica.com/40/144440-050-DA828627/Morgan-Freeman.jpg'
+        }
+        // call setPodcast
+        setPodcast(newPodcast)
+    }
+    getPodcast()
+  },[userContext.session]);
+
+  useEffect(() => {
     // You can update the track's duration when it's available in the context
     
   }, [audioContext.audioState.isPlaying]);
 
+  function padTo2Digits(number:number){
+    return number.toString().padStart(2, '0');
+  }
+
+    //TODO check if podcast is null, and use useEffect to change each time podcast changes.
   return (
     <View className='flex-1 items-center justify-center bg-secondary'>
-      <Image source={{ uri: tracks[0].artwork }} className='h-64 w-64' />
-      <Text className='mt-8 font-bold text-20'>{tracks[0].title}</Text>
-      <Text className='mt-4 text-20'>{tracks[0].artist}</Text>
+      <Image source={{ uri: podcast.artwork }} className='h-64 w-64' />
+      <Text className='mt-8 font-bold text-20'>{podcast.title}</Text>
+      <Text className='mt-4 text-20'>{podcast.artist}</Text>
       <View className='flex-row m-10'>
         <TouchableOpacity className='flex rounded-full' onPress={() => { audioContext.seekBackward(); }}>
           <Icon name="skip-previous" size={70} color="#00DEAD" />
