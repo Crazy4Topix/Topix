53233e456c877d85409a1ea0fc3e123e434179f7
Walter <walter.levens@gmail.com>
2024-01-05 12:39:57 +0100

    some more cleanup

--- app/(app)/profile.tsx
+++ app/(app)/profile.tsx
@@ -1,29 +1,29 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { View, Text, Pressable, StyleSheet, ToastAndroid} from 'react-native';
+import { View, Text, Pressable, ToastAndroid} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { signOut, getFullName } from '../../lib/supabase';
 import { SupabaseUserSession } from '../../contexts/user_session'
-import { supabase } from '../../lib/supabase';
 import { Icon } from 'react-native-elements';
 import { Dropdown } from 'react-native-element-dropdown';
 import { styled } from 'nativewind';
 import { Redirect } from 'expo-router';
-import { StringLiteral } from 'typescript';
-
-interface Voice{
-    id: string;
-    name: String;
-    display_name: string
-}
-
-const StyledDropdown = styled(Dropdown);
-
 
 export default function ProfilePage() {
     const navigation = useNavigation();
     const [fullName, setFullName] = useState('');
     const userContext = useContext(SupabaseUserSession);
     const userId = userContext.session?.user.id;
+
+    useEffect(() => {
+        const fetchFullName = async () => {
+            if (!userId) return
+
+            const name = await getFullName(userId);
+            setFullName(name)
+        };
+        
+        void fetchFullName();
+    }, [userId]);
 
     const handleLogout = async () => {
         try {
@@ -46,17 +46,6 @@ export default function ProfilePage() {
         navigation.navigate('updateVoice')
     }
 
-    useEffect(() => {
-        const fetchFullName = async () => {
-            if (!userId) return
-
-            const name = await getFullName(userId);
-            setFullName(name)
-        };
-        
-        void fetchFullName();
-    }, [userId]);
-
     if (fullName === null){
         ToastAndroid.show('Account is niet compleet, neem contact op met Crazy4.', ToastAndroid.LONG);
         return <Redirect href="/login" />;
@@ -71,24 +60,20 @@ export default function ProfilePage() {
             </View>
             <Text className="text-2xl font-semibold mb-4 text-center">Profiel Pagina</Text>
 
-      {/* Display Name */}
       <Text className="mb-4 font-primary text-xl">{fullName}</Text>
 
-      {/* Topic Selection Button */}
       <View className="mb-4 rounded-md bg-primary p-2">
         <Pressable onPress={navigateTopicSelection}>
           <Text className="font-primary text-white">Selecteer Topix</Text>
         </Pressable>
       </View>
 
-        {/* Voice Selection */}
       <View className="mb-4 rounded-md bg-primary p-2">
         <Pressable onPress={navigateVoiceSelection}>
           <Text className="font-primary text-white">Selecteer stem</Text>
         </Pressable>
       </View>
 
-      {/* Logout Button */}
       <Pressable onPress={handleLogout}>
         <View className="rounded-md bg-primary p-2 mb-4">
           <Text className="font-primary text-white">Uitloggen</Text>
